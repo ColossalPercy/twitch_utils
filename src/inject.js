38bb8eb5a0de1ac0f9beac6336de7e393ae2c38a
jackspercy <jackspercy@gmail.com>
2018-01-08 14:53:17 +0000

    more code

--- src/inject.js
+++ src/inject.js
@@ -1,2 +1,13 @@
-var src = 'http://rawgithub.com/colossalpercy/twitch_mod_tools/main.js';
-document.getElementsByTagName('head').
+var src;
+
+if (localStorage.tmtDev == 'true'){
+    src = 'http://127.0.0.1:3000/src/main.js';
+    console.log('TMT: Dev Environment Loaded');
+} else {
+    src = 'https://rawgit.com/ColossalPercy/twitch_mod_tools/master/src/main.js';
+}
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = src;
+document.head.append(script);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,43 @@
+var config = {
+    attributes: false,
+    childList: true,
+    characterData: false,
+    subtree: true
+};
+
+//Mutation observer for each chat message
+var chatObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        mutation.addedNodes.forEach(function(addedNode) {
+            console.log(addedNode);
+        });
+    });
+});
+
+//Mutation observer for chat loading
+var chatLoaded = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        var chatSelector = $(".chat-list");
+        if (chatSelector.length > 0) {
+            var target = chatSelector[0];
+            chatObserver.observe(target, config);
+        }
+    });
+});
+chatLoaded.observe($("body")[0], config);
+
+
+
+
+
+
+
+
+
+
+
+
+
 function doEvent( obj, type ) {
     /* Created by David@Refoua.me */
     var event = new Event( type, {target: obj, bubbles: true} );
@@ -15,4 +55,4 @@ function purge(){
     message('/timeout ' + name + ' 1');
 }
 
-$('[data-a-target="chat-purge-button"]').addEventListener('click',purge);
+//$('[data-a-target="chat-purge-button"]').addEventListener('click',purge);
