4545d7b7919a25d7b8f1ec043c73c2ac492029c2
jackspercy <jackspercy@gmail.com>
2018-04-20 16:59:33 +0100

    github twitch utils rebrand for loaders

--- chrome/inject.js
+++ chrome/inject.js
@@ -4,7 +4,7 @@ if (localStorage.tmtDev == 'true'){
     src = 'http://127.0.0.1:3000/build/tu.dev.js';
     console.log('TU: Dev Environment Loaded');
 } else {
-    src = 'https://rawgit.com/ColossalPercy/twitch_mod_tools/master/build/tu.min.js';
+    src = 'https://rawgit.com/ColossalPercy/twitch_utils/master/build/tu.min.js';
 }
 
 var script = document.createElement('script');
--- userscript/tmt.user.js
+++ userscript/tmt.user.js
@@ -17,7 +17,7 @@
         src = 'http://127.0.0.1:3000/build/tu.min.js';
         console.log('TU: Dev Environment Loaded');
     } else {
-        src = 'https://rawgit.com/ColossalPercy/twitch_mod_tools/master/build/tu.min.js';
+        src = 'https://rawgit.com/ColossalPercy/twitch_utils/master/build/tu.min.js';
     }
 
     var script = document.createElement('script');
